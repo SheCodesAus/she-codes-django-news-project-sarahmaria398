5bae562d128ef1e3cbe3c7a1724a3f4fb6328ce3
Sarah <sarahdavis398@gmail.com>
2022-06-06 19:17:54 +1000

    Created a View User Information Page, restricted site functionality
    - Created View User Information Page
    - Restricted ability to View User Information & Write New Story by checking if user is logged in, authenticated
    - If not logged in, provided option to either login or sign up, and links to those pages
    - Added navbar link to view user information

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreateAccountView
+from .views import CreateAccountView, ProfileView
 
 app_name = 'users'
 
 urlpatterns = [
     path('create-account/', CreateAccountView.as_view(), name='createAccount'),
+    path('profile/', ProfileView.as_view(), name='profile'),
     ]
--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy 
 from django.views.generic.edit import CreateView
 from django.views import generic
+from django.views.generic import TemplateView
 
 from .models import CustomUser
 from .forms import CustomUserCreationForm
@@ -10,3 +11,5 @@ class CreateAccountView(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'users/createAccount.html'
 
+class ProfileView(TemplateView):
+    template_name = 'users/profile.html'
