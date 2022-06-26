c20483fe0126f91e151a35be1b691301b3342443
Sarah <sarahdavis398@gmail.com>
2022-06-26 20:38:08 +1000

    Creating Category model

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,5 +1,13 @@
+from unicodedata import category
 from django.db import models
 from django.contrib.auth import get_user_model
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class NewsStory(models.Model):
@@ -11,6 +19,7 @@ class NewsStory(models.Model):
     pub_date = models.DateTimeField()
     content = models.TextField()
     image = models.CharField(max_length=400)
+    category = models.CharField(max_length=200, default='uncategorised')
 
     def __str__(self):
         return self.title
